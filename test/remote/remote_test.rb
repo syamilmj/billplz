bea4866c035fff2eb0a1312c21cad109f0a1d8e3
Syamil MJ <sy4mil@gmail.com>
2026-08-28 18:03:03 +0700

    Remove committed API key from test fixtures

--- test/remote/remote_test.rb
+++ test/remote/remote_test.rb
@@ -1,53 +1,57 @@
 require 'test_helper'
 
-# The following tests will send actual requests to the API
+# Hits the real Billplz sandbox. Excluded from the default rake task and from
+# CI; run it deliberately with `rake test:remote` and a sandbox key:
 #
-# It's being excluded from the :default rake task to minimise
-# risk of accidentally flooding the server with tests data
-# 
-# For sake of brevity, only one remote test class is created
+#   BILLPLZ_API_KEY=your-sandbox-key rake test:remote
+#
+# Skips itself when no key is set. Never point this at a production key.
 class RemoteTest < Minitest::Test
   def setup
+    WebMock.allow_net_connect!
+
+    @api_key = ENV['BILLPLZ_API_KEY']
+    skip 'Set BILLPLZ_API_KEY to a sandbox key to run the remote tests' if @api_key.nil? || @api_key.empty?
+
     Billplz.reset
-    Billplz.configuration.api_key = fixtures(:api_key)
+    Billplz.configuration.api_key = @api_key
+    Billplz.configuration.mode    = 'sandbox'
   end
 
-  def test_creates_new_collection_and_bill_then_delete
-    # create collection
-    collection = Billplz::Collection.new({ title: 'Just a mocked test' })
-    collection_response = collection.create
+  def teardown
+    WebMock.disable_net_connect!
+  end
 
-    assert collection.success?
-    assert_equal("JUST A MOCKED TEST", collection.parsed_json['title'])
+  def test_creates_new_collection_and_bill_then_deletes_it
+    collection = Billplz::Collection.new(title: 'Remote test collection')
+    collection.create
 
+    assert collection.success?, "collection create failed: #{collection.response.body}"
     collection_id = collection.parsed_json['id']
 
-    # create bill
-    bill = Billplz::Bill.new({ collection_id: collection_id, email: 'test@example.com', name: 'Test User', amount: 200, callback_url: 'http://example.com' })
-    bill_response = bill.create
+    bill = Billplz::Bill.new(
+      collection_id: collection_id,
+      email: 'test@example.com',
+      name: 'Test User',
+      amount: 200,
+      callback_url: 'https://example.com/callback',
+      description: 'Remote test bill'
+    )
+    bill.create
 
-    assert bill.success?
-    assert_equal(200, bill.parsed_json['amount'])
-
+    assert bill.success?, "bill create failed: #{bill.response.body}"
+    assert_equal 200, bill.parsed_json['amount']
     bill_id = bill.parsed_json['id']
 
-    # get bill
-    bill = Billplz::Bill.new({ bill_id: bill_id })
-    bill_response = bill.get
+    fetched = Billplz::Bill.new(bill_id: bill_id)
+    assert_equal bill_id, fetched.get['id']
 
-    assert bill.success?
-    assert_equal(bill_id, bill.parsed_json['id'])
+    deleted = Billplz::Bill.new(bill_id: bill_id)
+    deleted.delete
+    assert deleted.success?
 
-    # delete bill
-    bill = Billplz::Bill.new({ bill_id: bill_id })
-    bill.delete
-
-    assert bill.success?
-
-    # retrieve deleted bill
-    bill = Billplz::Bill.new({ bill_id: bill_id })
-    bill_response = bill.get
-
-    assert_instance_of(Net::HTTPNotFound, bill_response)
+    gone = Billplz::Bill.new(bill_id: bill_id)
+    assert_nil gone.get
+    assert_instance_of Net::HTTPNotFound, gone.response
   end
 end
